43ac75a79ad0f6174b673fc567fca3513b3eb221
Breno Martins <102875429+martnsBreno@users.noreply.github.com>
2022-12-23 14:24:45 -0300

    login feature

---
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,9 @@
+export default {
+
+    oidc: {
+        clientId: '0oa7qk3u9fOep8kOP5d7',
+        issuer: 'https://dev-91215888.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
